02c60e7a014ecffb92c7298f8b22f0f81c659761
tobozo <tobozo@users.noreply.github.com>
2024-10-02 17:53:46 +0200

    sort dirs with casefold

--- src/main.py
+++ src/main.py
@@ -41,7 +41,9 @@ def main():
                         "<a href=\"../\">../</a></th><td>-</td><td>-</td><td>-</td></tr>" if dirname != "." else "",
                         ]))
                 #sort dirnames alphabetically
-                dirnames.sort()
+                # dirnames.sort()
+                sorted_dirnames = sorted(dirnames, key=str.casefold)
+
                 for subdirname in dirnames:
                     f.write("<tr><th scope=\"row\"><img style=\"max-width:1rem; margin-right:.25rem\" src=\"" + get_icon_base64("o.folder") + "\"/>" + "<a href=\"" + subdirname + "/\">" +
                             subdirname + "/</a></th><td>-</td><td>-</td><td>-</td></tr>\n")
